838763f3d0a376e59c168acb7b34209351f11035
Ramesh A <ramesharpu@gmail.com>
2025-05-18 23:29:46 +0530

    maths operation

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -10,5 +10,10 @@ public class HelloWorld {
 	public void m1() {
 		System.out.println("from method-1");//from b11 branch dx cc + from c11 branch code change
 	}
+	
+	public void test() {
+		MathOperation obj = new MathOperation();
+		obj.add(25, 55);
+	}
 
 }
